14067e4dd5102b194d99edcb413d1892cccd1f46
matmott06 <matmot.bj@gmail.com>
2023-03-01 03:36:24 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 		while (y < 10)
 		{
 			z = x * y;
-			
+
 			if (z > 9)
 			{
 				_putchar(z / 10 + '0');
@@ -27,7 +27,7 @@ void times_table(void)
 			{
 				_putchar(' ');
 				_putchar(z + '0');
-			
+
 			}
 			else
 			{
